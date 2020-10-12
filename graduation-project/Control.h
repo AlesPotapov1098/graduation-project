7ba8a4d0d5e6a7a75f5b31c649d75250962b1438
Ales1098 <Ales1098@DESKTOP-GPAUOO3>
2020-10-12 09:53:21 +0700

    Добавил новый абстрактный класс для работы с контролами

---
+++ graduation-project/Control.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <Windows.h>
+
+#include "IGUIObject.h"
+#include "ColorScheme.h"
+
+namespace gui {
+	class Control : public IGUIObject
+	{
+	public:
+
+		Control() = default;
+		Control(HWND parent, LPCSTR text, __int32 width, __int32 heigth, __int32 x, __int32 y, LPCSTR ClassName, LPCSTR ButtonName);
+		~Control();
+
+		virtual void OnPaint() = 0;
+		 
+		LPCSTR GetText() const;
+		COLORREF GetBackColor() const;
+		COLORREF GetForeColor() const;
+		HWND GetParentHWND() const;
+
+	private:
+		HWND m_ParentHandle;
+		HFONT m_ControlTextFont;
+		COLORREF m_BackColor;
+		COLORREF m_ForeColor;
+		LPCSTR m_ControlText;
+	};
+}
