11d0b48356e9b0da758ae61bdfd6aa114e6ef0c8
Ales1098 <Ales1098@DESKTOP-GPAUOO3>
2020-10-12 09:55:25 +0700

    Завернул класс Control в новый namespace ctrl

--- graduation-project/Control.cpp
+++ graduation-project/Control.cpp
@@ -1,41 +1,41 @@
 #include "Control.h"
 
 namespace gui {
+	namespace ctrl {
+		Control::Control(HWND parent, LPCSTR text, __int32 width, __int32 heigth, __int32 x, __int32 y, LPCSTR ClassName, LPCSTR ButtonName)
+			: IGUIObject(width, heigth, x, y, ClassName, ButtonName)
+		{
+			m_ParentHandle = parent;
+			m_ControlText = text;
 
-	Control::Control(HWND parent, LPCSTR text, __int32 width, __int32 heigth, __int32 x, __int32 y, LPCSTR ClassName, LPCSTR ButtonName)
-	: IGUIObject(width,heigth,x,y,ClassName,ButtonName)
-	{
-		m_ParentHandle = parent;
-		m_ControlText = text;
+			m_BackColor = color::WHITE;
+			m_ForeColor = color::BLACK;
+			m_ControlTextFont = nullptr;
+		}
 
-		m_BackColor = color::WHITE;
-		m_ForeColor = color::BLACK;
-		m_ControlTextFont = nullptr;
+		Control::~Control()
+		{
+			CloseHandle(m_ControlTextFont);
+		}
+
+		LPCSTR Control::GetText() const
+		{
+			return m_ControlText;
+		}
+
+		COLORREF Control::GetBackColor() const
+		{
+			return m_BackColor;
+		}
+
+		COLORREF Control::GetForeColor() const
+		{
+			return m_ForeColor;
+		}
+
+		HWND Control::GetParentHWND() const
+		{
+			return m_ParentHandle;
+		}
 	}
-
-	Control::~Control()
-	{
-		CloseHandle(m_ControlTextFont);
-	}
-
-	LPCSTR Control::GetText() const
-	{
-		return m_ControlText;
-	}
-
-	COLORREF Control::GetBackColor() const
-	{
-		return m_BackColor;
-	}
-
-	COLORREF Control::GetForeColor() const
-	{
-		return m_ForeColor;
-	}
-
-	HWND Control::GetParentHWND() const
-	{
-		return m_ParentHandle;
-	}
-
 }
--- graduation-project/Control.h
+++ graduation-project/Control.h
@@ -5,26 +5,28 @@
 #include "ColorScheme.h"
 
 namespace gui {
-	class Control : public IGUIObject
-	{
-	public:
+	namespace ctrl {
+		class Control : public IGUIObject
+		{
+		public:
 
-		Control() = default;
-		Control(HWND parent, LPCSTR text, __int32 width, __int32 heigth, __int32 x, __int32 y, LPCSTR ClassName, LPCSTR ButtonName);
-		~Control();
+			Control() = default;
+			Control(HWND parent, LPCSTR text, __int32 width, __int32 heigth, __int32 x, __int32 y, LPCSTR ClassName, LPCSTR ButtonName);
+			~Control();
 
-		virtual void OnPaint() = 0;
-		 
-		LPCSTR GetText() const;
-		COLORREF GetBackColor() const;
-		COLORREF GetForeColor() const;
-		HWND GetParentHWND() const;
+			virtual void OnPaint() = 0;
 
-	private:
-		HWND m_ParentHandle;
-		HFONT m_ControlTextFont;
-		COLORREF m_BackColor;
-		COLORREF m_ForeColor;
-		LPCSTR m_ControlText;
-	};
+			LPCSTR GetText() const;
+			COLORREF GetBackColor() const;
+			COLORREF GetForeColor() const;
+			HWND GetParentHWND() const;
+
+		private:
+			HWND m_ParentHandle;
+			HFONT m_ControlTextFont;
+			COLORREF m_BackColor;
+			COLORREF m_ForeColor;
+			LPCSTR m_ControlText;
+		};
+	}
 }
