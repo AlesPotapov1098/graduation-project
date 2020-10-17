296f2a16cb2adf2efd3e36a3aab69ddcbaa04316
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2020-10-17 13:10:17 +0700

    Описал программную модель

--- graduation-project/gpgpu/OpenCLProgramUnit.h
+++ graduation-project/gpgpu/OpenCLProgramUnit.h
@@ -1,32 +1,48 @@
 #pragma once
 
+#include <string>
+
 #include "IOpenCLObject.h"
 
 namespace gpgpu {
 
 	namespace objects {
 
-		template<typename T>
+		template<typename T, typename L>
 		class OpenCLProgramUnit : public IOpenCLObject<T> {
 		public:
 			OpenCLProgramUnit() = default;
 			virtual ~OpenCLProgramUnit() = default;
 
-
+			virtual bool Load(const L&) = 0;
+			virtual bool Build() = 0;
+			virtual const std::string& ErrorMessage() = 0;
+		protected:
+			L m_Obj;
 		};
 
-		class OpenCLProgram : public OpenCLProgramUnit<cl_program> {
+		class OpenCLProgram : public OpenCLProgramUnit<cl_program, std::string> {
+		public:
+			OpenCLProgram();
+			~OpenCLProgram();
 
+			bool Load(const std::string& src) override;
+			bool Build() override;
+			const std::string ErrorMessage() override;
+
+		private:
+			std::string m_SRC;
 		};
 
-		class OpenCLKernel : public OpenCLProgramUnit<cl_kernel> {
+		class OpenCLKernel : public OpenCLProgramUnit<cl_kernel, OpenCLProgram> {
+		public:
+			OpenCLKernel();
+			~OpenCLKernel();
 
+			bool Load(const OpenCLProgram& program) override;
+			bool Build() override;
+			const std::string ErrorMessage() override;
 		};
-
-		class OpenCLSampler : public OpenCLProgramUnit<cl_sampler> {
-
-		};
-
 
 	}
 
