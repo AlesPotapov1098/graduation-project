d16c0e0226884e4d164d10a66590979c70022e90
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2020-10-20 10:20:21 +0700

    Добавил новый файл, в ктором будет абстрактный класс для файлов лога и настроек

--- graduation-project/main.cpp
+++ graduation-project/main.cpp
@@ -9,36 +9,35 @@
 #include <iostream>
 #include <CL/opencl.hpp>
 #include <string>
+#include <vector>
 
 int main()
 {
-	cl::vector<cl::Platform> platforms;
-	cl::Platform::get(&platforms);
+	try {
+		std::vector<cl::Platform> platforms;
+		cl::Platform::get(&platforms);
 
-	for (auto& platfrom : platforms)
-	{
-		auto platform_name = platfrom.getInfo<CL_PLATFORM_NAME>();
-
-		std::cout << platform_name << std::endl;
-
-		cl::vector<cl::Device> devices;
-
-		platfrom.getDevices(CL_DEVICE_TYPE_ALL, &devices);
-		
-		for (auto& device : devices)
+		for (auto& platfrom : platforms)
 		{
-			auto device_name = device.getInfo<CL_DEVICE_NAME>();
-
-			std::cout << device_name << std::endl;
-
-			cl::string device_vendor = device.getInfo<CL_DEVICE_VENDOR>();
-
-			std::cout << device_vendor << std::endl;
-
-			cl::string device_ext = device.getInfo<CL_DEVICE_EXTENSIONS>();
-
-			std::cout << device_ext << std::endl;
+			auto platform_name = platfrom.getInfo<CL_PLATFORM_NAME>();
+			std::cout << platform_name << std::endl;
+			std::string platform_ext = platfrom.getInfo<CL_PLATFORM_EXTENSIONS>();
+			std::cout << platform_ext << std::endl;
+			std::vector<cl::Device> devices;
+			platfrom.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+			for (auto& device : devices)
+			{
+				auto device_name = device.getInfo<CL_DEVICE_NAME>();
+				std::cout << device_name << std::endl;
+				cl::string device_vendor = device.getInfo<CL_DEVICE_VENDOR>();
+				std::cout << device_vendor << std::endl;
+				cl::string device_ext = device.getInfo<CL_DEVICE_EXTENSIONS>();
+				std::cout << device_ext << std::endl;
+			}
 		}
+	}
+	catch (cl::Error error) {
+		std::cout << error.what() << std::endl;
 	}
 
 	return 0;
